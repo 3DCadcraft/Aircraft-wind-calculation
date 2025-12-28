73b06d8861c2048751beb790c282422c32f7609b
3DCadcraft <vtt495@yahoo.fr>
2025-12-28 22:05:30 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vent-ifr-cache-v2';
+const CACHE_NAME = 'vent-ifr-cache-v3';
 const urlsToCache = [
   './',
   'index.html',
@@ -6,7 +6,6 @@ const urlsToCache = [
   'icon-192.png',
   'icon-512.png'
 ];
-
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -16,16 +15,14 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     )
   );
 });
